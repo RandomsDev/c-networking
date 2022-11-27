ff2980ccac92e2bc325d3646613e94b84ce62284
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-11-27 22:01:37 +0500

    commit via GitHub-CLI

--- github.cpp
+++ github.cpp
@@ -2,6 +2,8 @@
 #include <map>
 #include <iostream>
 
+#define NEED_HELP 200
+
 enum class ArgEnum
 {
 	commit = 0,
@@ -11,8 +13,11 @@ enum class ArgEnum
 
 int main(int argc, char* argv[])
 {
-	std::cout << argv [1] << " \n";
-	if (argc > 1 && argv [1] == "--help")
+	try 
+	{
+		if (argc >  1 && std::string (argv [1]) == "--help") throw NEED_HELP;
+	}
+	catch (int)
 	{
 		system ("echo github [commit-message=\"commit via GitHub-CLI\"] [branch=\"master\"] [remote=\"origin\"]");
 		return EXIT_SUCCESS;
@@ -20,17 +25,19 @@ int main(int argc, char* argv[])
 
 	std::map <ArgEnum, std::string> argMap;
 	
-	argMap [ArgEnum::commit] =  "commit via GitHub-CLI";
+	argMap [ArgEnum::commit] =  "\"commit via GitHub-CLI\"";
 	argMap [ArgEnum::branch] =  "master";
 	argMap [ArgEnum::remote] =  "origin";
 	
 	for (int i = (int)ArgEnum::commit; i != (int)ArgEnum::remote; i += 1)
 	{
-		if (argc > i + 1) argMap [static_cast <ArgEnum> (i)] = argv [i];
+		if (argc > i + 1) argMap [static_cast <ArgEnum> (i)] = std::string(argv [i]);
 	}
 
 	std::string command = "git add . && git commit -m " + argMap [ArgEnum::commit] +
 	" && git push " + argMap [ArgEnum::remote] + " " +argMap [ArgEnum::remote];
+	
+	std::cout << command << "\n";
 	system (command.c_str ()); 
 	return EXIT_SUCCESS;
 }
