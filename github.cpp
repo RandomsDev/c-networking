5e321037051fc845423edefe567dcb58646117a4
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-11-27 22:10:33 +0500

    commit via GitHub-CLI

--- github.cpp
+++ github.cpp
@@ -29,7 +29,7 @@ int main(int argc, char* argv[])
 	argMap [ArgEnum::branch] =  "master";
 	argMap [ArgEnum::remote] =  "origin";
 	
-	argc -= 1;
+	
 	for (int i = 0 ; i < argc ; i += 1)
 	{
 		if (i != 0 && i < argMap.size ())
